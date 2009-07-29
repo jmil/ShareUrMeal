144eb21c936db07ebf172d461199e7d33ee8d6b5
Jordan Miller <peptides@benchscience.org>
2009-07-28 20:06:50 -0400

    xcode project template; builds correctly

---
+++ Classes/ShareUrMealAppDelegate.h
@@ -0,0 +1,29 @@
+//
+//  ShareUrMealAppDelegate.h
+//  ShareUrMeal
+//
+//  Created by Jordan on 7/28/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+@interface ShareUrMealAppDelegate : NSObject <UIApplicationDelegate> {
+
+    NSManagedObjectModel *managedObjectModel;
+    NSManagedObjectContext *managedObjectContext;	    
+    NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+    UIWindow *window;
+}
+
+- (IBAction)saveAction:sender;
+
+@property (nonatomic, retain, readonly) NSManagedObjectModel *managedObjectModel;
+@property (nonatomic, retain, readonly) NSManagedObjectContext *managedObjectContext;
+@property (nonatomic, retain, readonly) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+@property (nonatomic, readonly) NSString *applicationDocumentsDirectory;
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
+
