d2da233e020b3baf74f063710d3756ea4e872a44
Andy Mroczkowski <andy@mrox.net>
2009-08-25 21:04:10 -0400

    basic about page loading 	loads from standard 'about' URL 	if fails, falls back to local about.html

--- Classes/AboutViewController.m
+++ Classes/AboutViewController.m
@@ -1,5 +1,43 @@
 #import "AboutViewController.h"
 
+//@interface AboutViewController ()
+//@end
+
+
 @implementation AboutViewController
+
+@synthesize webView = _webView;
+
+- (void) viewDidLoad
+{
+	[super viewDidLoad];
+
+	NSURLRequest* request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://staging.shareurmeal.com/about"]];
+	[self.webView loadRequest:request];
+	[self.webView setScalesPageToFit:YES];
+	[[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:YES];
+
+}
+
+
+- (void) webViewDidFinishLoad:(UIWebView*)webView
+{
+	[[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+}
+
+
+- (void)webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+	[[UIApplication sharedApplication] setNetworkActivityIndicatorVisible:NO];
+	
+	// -- load fallback content
+	
+	NSString* localAboutPath = [[NSBundle mainBundle] pathForResource:@"about" ofType:@"html"];
+	NSURL* localAboutURL = [NSURL fileURLWithPath:localAboutPath];
+	
+	NSURLRequest* request = [NSURLRequest requestWithURL:localAboutURL];
+	[webView loadRequest:request];
+}
+
 
 @end
